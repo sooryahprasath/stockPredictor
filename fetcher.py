deddf62ae7c6aeecbb877668c832eda790b8af1f
Sooryah Prasath <sooryahprasath@gmail.com>
2019-10-24 14:10:04 +0530

    Basic Predictor

--- fetcher.py
+++ fetcher.py
@@ -1,37 +1,44 @@
 #Install the dependencies
 import quandl
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 from sklearn.model_selection import train_test_split
-
+import datetime
 
 #Get the stock data
-df = quandl.get("WIKI/AMZN")
+ticker = input("Enter ticker: ")
+df = quandl.get("EOD/"+ticker, api_key="PVFkPt8nSNtyGxSFUaSm")
+#Plot a intial graph
+df.Close.plot()
+plt.title(ticker)
+plt.show()
+
+df = df[['Close']]
+print(df.head())
 
 # A variable for predicting 'n' days out into the future
-#'n=30' days
-forecast_out = 30
+forecast_out = 50
 
-#Create another column (the target or dependent variable) shifted 'n' units up
-df['Prediction'] = df[['Adj. Close']].shift(-forecast_out)
+#Create another column (the target ) shifted 'n' units up
+df['Prediction'] = df[['Close']].shift(-forecast_out)
 
 #print the new data set
 print(df.tail())
 
-### Create the indeoendent data set (X)  #######
+### Create the independent data set (X)  #######
 # Convert the dataframe to a numpy array
-X = np.array(df.drop(['Prediction'], 1))
+X = np.array(df.drop(['Prediction'],1))
 
-#Remove the last 'n' rows
+#Remove the last '30' rows
 X = X[:-forecast_out]
 print(X)
 
 ### Create the dependent data set (y)  #####
-# Convert the dataframe to a numpy array (All of the values including the NaN's)
+# Convert the dataframe to a numpy array
 y = np.array(df['Prediction'])
-
-# Get all of the y values except the last 'n' rows
+# Get all of the y values except the last '30' rows
 y = y[:-forecast_out]
 print(y)
 
@@ -42,15 +49,8 @@ x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
 svr_rbf.fit(x_train, y_train)
 
-# Testing Model: Score returns the coefficient of determination R^2 of the prediction.
-# The best possible score is 1.0
-svm_confidence = svr_rbf.score(x_test, y_test)
-print("svm confidence: ", svm_confidence)
-
-
 # Create and train the Linear Regression  Model
 lr = LinearRegression()
-
 # Train the model
 lr.fit(x_train, y_train)
 
@@ -59,20 +59,46 @@ lr.fit(x_train, y_train)
 lr_confidence = lr.score(x_test, y_test)
 print("lr confidence: ", lr_confidence)
 
-
 # Set x_forecast equal to the last 30 rows of the original data set from Adj. Close column
-x_forecast = np.array(df.drop(['Prediction'] ,1))[-forecast_out:]
+x_forecast = np.array(df.drop(['Prediction'],1))[-forecast_out:]
 print(x_forecast)
 
-# Print linear regression model predictions for the next 'n' days
+# Print linear regression model predictions for the next '30' days
 lr_prediction = lr.predict(x_forecast)
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in lr_prediction:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+df['Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
 print(lr_prediction)
 
-# Print support vector regressor model predictions for the next 'n' days
+# Print support vector regressor model predictions for the next '30' days
 svm_prediction = svr_rbf.predict(x_forecast)
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in svm_prediction:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+df['Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+#plt.show()
 print(svm_prediction)
-
-
-
-
-
